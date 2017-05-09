6012e35943d648f425da68b7d54f19054b6dfdb0
adrian83 <adrianbrzoza1983@gmail.com>
2017-05-09 22:23:09 +0200

    ex 2.1 updated

--- src/main/scala/chapter02/Exercise_02_01.scala
+++ src/main/scala/chapter02/Exercise_02_01.scala
@@ -1,18 +1,18 @@
 package chapter02
 
 object Exercise_02_01 extends App {
-  
+
   def fib(n: Int): Int = {
-    @annotation.tailrec def f(first: Int, second: Int, index: Int): Int = 
-      if(index == 0) first + second else f(second, first + second, index-1)
-    
-    n match {
-      case 1 => 0
-      case 2 => 1
-      case _ => f(0, 1, n-3)
-    }
+    @annotation.tailrec def f(first: Int, second: Int, n: Int): Int =
+      n match {
+        case 0 => first + second
+        case 1 => first
+        case 2 => second
+        case _ => f(second, first + second, n - 1)
+      }
+    f(0, 1, n)
   }
-  
+
   1 until 40 map fib foreach println
-  
+
 }
