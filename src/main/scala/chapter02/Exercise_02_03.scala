ef046cd09c1165cd352765fab98bb0c5b10b3f11
adrian83 <adrianbrzoza1983@gmail.com>
2017-05-10 18:27:32 +0200

    ex 2.3 added

---
+++ src/main/scala/chapter02/Exercise_02_03.scala
@@ -0,0 +1,10 @@
+package chapter02
+
+object Exercise_02_03 extends App {
+  
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = (a:A) => (b:B) => f(a,b)
+  
+  println(curry((a:Int, b:Int) => a+b)(3)(5))
+  
+  
+}
