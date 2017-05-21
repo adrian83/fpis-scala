aeb59324285f85f2148e900a3e9e82a1f04f003e
adrian83 <adrianbrzoza1983@gmail.com>
2017-05-21 21:50:54 +0200

    ex 3.23 added

---
+++ src/main/scala/chapter03/Exercise_03_23.scala
@@ -0,0 +1,12 @@
+package chapter03
+
+object Exercise_03_23 extends App {
+  
+  def zipWith[A, B](l1: List[A], l2: List[A])(f: (A, A) => B): List[B] = l1 match {
+    case x :: xs => f(x, l2.head) :: zipWith(xs, l2.tail)(f)
+    case Nil => Nil
+  }
+  
+  println(zipWith(List(1,2,3), List(4,5,6))((a1, a2) => a1.toString + a2.toString))
+  
+}
